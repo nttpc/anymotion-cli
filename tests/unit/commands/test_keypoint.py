5ece1f9a60f969ea376691307cc2421efc8776b5
Yusuke Kumihashi <y_kumiha@nttpc.co.jp>
2019-12-23 18:49:13 +0900

    テストを追加

--- tests/unit/commands/test_keypoint.py
+++ tests/unit/commands/test_keypoint.py
@@ -1,4 +1,5 @@
 from click.testing import CliRunner
+import textwrap
 
 from encore_api_cli.client import Client
 from encore_api_cli.commands.keypoint import cli
@@ -30,3 +31,60 @@ def test_keypoint_list(mocker, requests_mock):
 
     assert result.exit_code == 0
     assert result.output == '[]\n'
+
+
+def test_keypoint_extract(mocker):
+    client_mock = mocker.MagicMock()
+    client_mock.return_value.extract_keypoint.return_value = 2
+    mocker.patch('encore_api_cli.commands.keypoint.get_client', client_mock)
+
+    runner = CliRunner()
+    result = runner.invoke(cli, ['keypoint', 'extract', '--image_id', '1'])
+
+    kwargs = client_mock.return_value.extract_keypoint.call_args.kwargs
+
+    assert client_mock.call_count == 1
+    assert client_mock.return_value.extract_keypoint.call_count == 1
+    assert kwargs == {'image_id': 1, 'movie_id': None}
+
+    assert result.exit_code == 0
+
+
+def test_keypoint_extract_error(mocker):
+    runner = CliRunner()
+    result = runner.invoke(cli, ['keypoint', 'extract'])
+
+    assert result.exit_code == 2
+    assert result.output == textwrap.dedent("""\
+        Usage: cli keypoint extract [OPTIONS]
+
+        Error: Either "movie_id" or "image_id" is required.
+    """)
+
+
+def test_keypoint_extract_with_drawing(mocker):
+    image_id = 1
+    keypoint_id = 2
+
+    client_mock = mocker.MagicMock()
+    client_mock.return_value.extract_keypoint.return_value = keypoint_id
+    client_mock.return_value.draw_keypoint.return_value = 'url'
+    client_mock.return_value.download.return_value = None
+    mocker.patch('encore_api_cli.commands.keypoint.get_client', client_mock)
+
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['keypoint', 'extract', '--image_id', image_id, '--with_drawing'])
+
+    assert client_mock.call_count == 1
+    assert client_mock.return_value.extract_keypoint.call_count == 1
+    assert client_mock.return_value.extract_keypoint.call_args.kwargs == {
+        'image_id': image_id,
+        'movie_id': None
+    }
+    assert client_mock.return_value.draw_keypoint.call_count == 1
+    assert client_mock.return_value.draw_keypoint.call_args.args == (
+        keypoint_id,)
+    assert client_mock.return_value.download.call_count == 1
+
+    assert result.exit_code == 0
