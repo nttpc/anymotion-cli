3612ad085f3ab58b57d64eca7cd4b583f99af77c
Yusuke Kumihashi <y_kumiha@nttpc.co.jp>
2020-02-10 19:05:06 +0900

    add client test

--- tests/unit/sdk/test_client.py
+++ tests/unit/sdk/test_client.py
@@ -4,7 +4,7 @@ from urllib.parse import urljoin
 import pytest
 
 from encore_api_cli.sdk.client import Client
-from encore_api_cli.sdk.exceptions import FileTypeError
+from encore_api_cli.sdk.exceptions import ClientValueError, FileTypeError
 
 
 @pytest.fixture
@@ -14,6 +14,37 @@ def client(requests_mock):
     oauth_url = urljoin(client._base_url, "v1/oauth/accesstokens")
     requests_mock.post(oauth_url, json={"accessToken": "token"})
     yield client
+
+
+class TestInit(object):
+    @pytest.mark.parametrize(
+        "api_url",
+        ["http://api.example.com/anymotion/v1/", "http://api.example.com/anymotion/v1"],
+    )
+    def test_valid(self, api_url):
+        client = Client("client_id", "client_secret", api_url)
+        assert isinstance(client, Client)
+        assert client._api_url == "http://api.example.com/anymotion/v1/"
+
+    @pytest.mark.parametrize(
+        "client_id, client_secret, api_url, expected",
+        [
+            ("", "", "", "Invalid Client ID: "),
+            ("client_id", "", "", "Invalid Client Secret: "),
+            ("client_id", "client_secret", "", "Invalid API URL: "),
+            (
+                "client_id",
+                "client_secret",
+                "http://api.example.com/",
+                "Invalid API URL: http://api.example.com/",
+            ),
+        ],
+    )
+    def test_invalid(self, client_id, client_secret, api_url, expected):
+        with pytest.raises(ClientValueError) as excinfo:
+            Client(client_id, client_secret, api_url)
+
+        assert str(excinfo.value) == expected
 
 
 class TestGetData(object):
@@ -174,3 +205,29 @@ def test_ファイルをダウンロードできること(mocker, requests_mock,
     client.download(url, path)
 
     assert path.exists()
+
+
+@pytest.mark.parametrize("image_id, movie_id", [(3, None), (None, 4)])
+def test_get_name_from_drawing_id(requests_mock, client, image_id, movie_id):
+    drawing_id = 1
+    keypoint_id = 2
+    expected = "file_name"
+
+    requests_mock.get(
+        f"{client._api_url}drawings/{drawing_id}/",
+        json={"execStatus": "SUCCESS", "keypoint": keypoint_id},
+    )
+    requests_mock.get(
+        f"{client._api_url}keypoints/{keypoint_id}/",
+        json={"execStatus": "SUCCESS", "image": image_id, "movie": movie_id},
+    )
+    requests_mock.get(
+        f"{client._api_url}images/{image_id}/", json={"name": expected},
+    )
+    requests_mock.get(
+        f"{client._api_url}movies/{movie_id}/", json={"name": expected},
+    )
+
+    name = client.get_name_from_drawing_id(drawing_id)
+
+    assert name == expected
